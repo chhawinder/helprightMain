69af2da5a534fceaafafa2cba84ba96df592f812
chhawinder <chhawinder.1465@gmail.com>
2023-02-24 09:26:56 +0530

    Updated

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -25,34 +25,6 @@ function HomePage() {
        </div>
        
 
-
-     
-        <section className="hero">
-          <h1>Welcome to Our Website</h1>
-          <p>Discover what we have to offer</p>
-          <button>Learn More</button>
-        </section>
-        <section className="services">
-          <h2>Our Services</h2>
-          <ul>
-            <li><span>Service 1</span></li>
-            <li><span>Service 2</span></li>
-            <li><span>Service 3</span></li>
-          </ul>
-        </section>
-        <section className="testimonials">
-          <h2>Testimonials</h2>
-          <ul>
-            <li>
-              <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
-              <span>- John Doe, Client</span>
-            </li>
-            <li>
-              <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
-              <span>- Jane Smith, Client</span>
-            </li>
-          </ul>
-        </section>
       </main>
       <footer>
         <p>&copy; 2023 Our Company</p>
--- src/components/carousel.js
+++ src/components/carousel.js
@@ -3,41 +3,41 @@ import React, { useState, useEffect } from 'react';
 import './carousel.css';
 
 function Carousel() {
-    const [activeIndex, setActiveIndex] = useState(0);
+  const [activeIndex, setActiveIndex] = useState(0);
 
-    const slides = [
-      {
-        id: 1,
-        imgUrl: 'https://images.unsplash.com/photo-1532629345422-7515f3d16bb6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80', 
-        caption: 'Slide 1',
-      },
-      {
-        id: 2,
-        imgUrl: 'https://images.unsplash.com/photo-1494386346843-e12284507169?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80',
-        caption: 'Slide 2',
-      },
-      {
-        id: 3,
-        imgUrl: 'https://images.unsplash.com/photo-1609139003551-ee40f5f73ec0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-        caption: 'Slide 3',
-      },
-    ];
-  
-    const handlePrev = () => {
-      setActiveIndex(activeIndex === 0 ? slides.length - 1 : activeIndex - 1);
-    };
-  
-    const handleNext = () => {
-      setActiveIndex(activeIndex === slides.length - 1 ? 0 : activeIndex + 1);
-    };
+  const slides = [
+    {
+      id: 1,
+      imgUrl: 'https://images.unsplash.com/photo-1532629345422-7515f3d16bb6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80', 
+      caption: 'Slide 1',
+    },
+    {
+      id: 2,
+      imgUrl: 'https://images.unsplash.com/photo-1494386346843-e12284507169?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80',
+      caption: 'Slide 2',
+    },
+    {
+      id: 3,
+      imgUrl: 'https://images.unsplash.com/photo-1609139003551-ee40f5f73ec0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+      caption: 'Slide 3',
+    },
+  ];
 
-    return(
-        <div className="carousel">
-        <img src={slides[activeIndex].imgUrl} alt={slides[activeIndex].caption} />
-        <button className="prev" onClick={handlePrev}>&#10094;</button>
-        <button className="next" onClick={handleNext}>&#10095;</button>
-      </div>
-    );
+  const handlePrev = () => {
+    setActiveIndex(activeIndex === 0 ? slides.length - 1 : activeIndex - 1);
+  };
+
+  const handleNext = () => {
+    setActiveIndex(activeIndex === slides.length - 1 ? 0 : activeIndex + 1);
+  };
+
+  return(
+      <div className="carousel">
+      <img src={slides[activeIndex].imgUrl} alt={slides[activeIndex].caption} />
+      <button className="prev" onClick={handlePrev}>&#10094;</button>
+      <button className="next" onClick={handleNext}>&#10095;</button>
+    </div>
+  );
 
 
 }
